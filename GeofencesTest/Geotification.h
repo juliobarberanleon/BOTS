c42425cfca6556d74bc38080917d3196d8ca2376
JULIO BARBERAN <juliobarberan@Mac-Pro-de-JULIO.local>
2015-11-06 21:18:40 -0500

    reinitialized files

---
+++ GeofencesTest/Geotification.h
@@ -0,0 +1,25 @@
+//
+//  Geotification.h
+//  GeofencesTest
+//
+//  Created by Guillermo Saenz on 6/14/15.
+//  Copyright (c) 2015 Property Atomic Strong SAC. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@import MapKit;
+@import CoreLocation;
+
+
+
+@interface Geotification : NSObject <NSCoding, MKAnnotation>
+
+@property (nonatomic, assign) CLLocationCoordinate2D coordinate;
+@property (nonatomic, assign) CLLocationDistance radius;
+@property (nonatomic, strong) NSString *identifier;
+@property (nonatomic, strong) NSString *note;
+
+- (instancetype)initWithCoordinate:(CLLocationCoordinate2D)coordinate radius:(CLLocationDistance)radius identifier:(NSString *)identifier note:(NSString *)note;
+
+@end
---
+++ GeofencesTestTests/GeofencesTestTests.m
@@ -0,0 +1,49 @@
+//
+//  GeofencesTestTests.m
+//  GeofencesTestTests
+//
+//  Created by Guillermo Saenz on 6/14/15.
+//  Copyright (c) 2015 Property Atomic Strong SAC. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <XCTest/XCTest.h>
+#import "GeotificationsViewController.h"
+#import "AddGeotificationViewController.h"
+#import "Geotification.h"
+
+@interface GeofencesTestTests : XCTestCase
+
+@end
+
+@implementation GeofencesTestTests
+
+- (void)setUp {
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    XCTAssert(YES, @"Pass");
+}
+
+
+- (void)testLoad {
+    
+    NSArray *savedItems = [[NSUserDefaults standardUserDefaults] arrayForKey:kSavedItemsKey];
+    if (savedItems) {
+        for (id savedItem in savedItems) {
+            Geotification *geotification = [NSKeyedUnarchiver unarchiveObjectWithData:savedItem];
+            XCTAssertEqualObjects(geotification.note, @"Ecuador - Manta");
+        }
+    }
+}
+
+
+@end
